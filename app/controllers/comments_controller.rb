0c44c08f18beb9766717335cf648db8b19082e08
dguilar1994 <andrea@MacBook-Pro-de-Andrea.local>
2017-07-30 14:24:39 -0500

    Add comments via normal request is working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def index
+    post = Post.find params[:post_id]
+    @comments = post.comments
+    render json: @comments
+  end
+  def create
+    @comment = Comment.new (safe_params)
+    @post = Post.find params[:post_id]
+    @user = @post.user
+    @comment.user_id = @user.id
+    @comment.save
+    @post.comments.push @comment
+    redirect_to user_post_path(@user, @post)
+  end
+
+  protected
+  def safe_params
+      params.require(:comment).permit(:content, :post_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :posts, only: [:index, :show] 
+  resources :posts, only: [:index, :show] do
+    resources :comments, only: [:index, :create]
+  end
 
   resources :users, except: [:index, :create, :new, :show] do
     resources :posts
